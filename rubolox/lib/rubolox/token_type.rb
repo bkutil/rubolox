e08986b90d2094df7d2d4f04335ad086551ea0c1
Balazs Kutil <balazs@kutilovi.cz>
2021-09-05 07:07:54 +0200

    Modify the token_type enum to use constants

--- rubolox/lib/rubolox/token_type.rb
+++ rubolox/lib/rubolox/token_type.rb
@@ -1,50 +1,52 @@
 module Rubolox
-  # Ruby doesn't have enums, and some keywords are reserved in Ruby, so let's
-  # use a hash.
-  TokenType = {
-    # Single-character tokens.
-    LEFT_PAREN:     1,
-    RIGHT_PAREN:    2,
-    LEFT_BRACE:     3,
-    RIGHT_BRACE:    4,
-    COMMA:          5,
-    DOT:            6,
-    MINUS:          7,
-    PLUS:           8,
-    SEMICOLON:      9,
-    SLASH:          10,
-    STAR:           11,
-    # One or two character tokens.
-    BANG:           12,
-    BANG_EQUAL:     13,
-    EQUAL:          14,
-    EQUAL_EQUAL:    15,
-    GREATER:        16,
-    GREATER_EQUAL:  17,
-    LESS:           18,
-    LESS_EQUAL:     19,
-    # Literals
-    IDENTIFIER:     20,
-    STRING:         21,
-    NUMBER:         22,
-    # Keywords
-    END:            23,
-    CLASS:          24,
-    ELSE:           25,
-    FALSE:          26,
-    FUN:            27,
-    FOR:            28,
-    IF:             29,
-    NIL:            30,
-    OR:             31,
-    PRINT:          32,
-    RETURN:         33,
-    SUPER:          34,
-    THIS:           35,
-    TRUE:           36,
-    VAR:            37,
-    WHILE:          38,
+  module TokenType
+    [
+      # Single-character tokens.
+      :LEFT_PAREN,
+      :RIGHT_PAREN,
+      :LEFT_BRACE,
+      :RIGHT_BRACE,
+      :COMMA,
+      :DOT,
+      :MINUS,
+      :PLUS,
+      :SEMICOLON,
+      :SLASH,
+      :STAR,
+      # One or two character tokens.
+      :BANG,
+      :BANG_EQUAL,
+      :EQUAL,
+      :EQUAL_EQUAL,
+      :GREATER,
+      :GREATER_EQUAL,
+      :LESS,
+      :LESS_EQUAL,
+      # Literals
+      :IDENTIFIER,
+      :STRING,
+      :NUMBER,
+      # Keywords
+      :END,
+      :CLASS,
+      :ELSE,
+      :FALSE,
+      :FUN,
+      :FOR,
+      :IF,
+      :NIL,
+      :OR,
+      :PRINT,
+      :RETURN,
+      :SUPER,
+      :THIS,
+      :TRUE,
+      :VAR,
+      :WHILE,
 
-    EOF:            39,
-  }.freeze
+      :EOF,
+    ].each.with_index do |keyword, i|
+      const_set(keyword, i)
+    end
+  end
 end
