cc014babf02c5a15d107f3325dc8088b233f2724
Balazs Kutil <balazs@kutilovi.cz>
2021-09-13 23:51:52 +0200

    Add AST printer
    for testing the generated expression code. It will be removed in
    subsequent commit.
    
    Usage:
    
    require_relative 'lib/rubolox'
    require_relative 'lib/rubolox/ast_printer'
    
    exprs = Rubolox::Expr::Binary.new(
    		Rubolox::Expr::Unary.new(
    			Rubolox::Token.new(Rubolox::TokenType::MINUS, "-", nil, 1),
    			Rubolox::Expr::Literal.new(123)
    		),
    		Rubolox::Token.new(Rubolox::TokenType::STAR, "*", nil, 1),
    		Rubolox::Expr::Grouping.new(
    			Rubolox::Expr::Literal.new(45.67)
    		)
    	)
    
    printer = Rubolox::AstPrinter.new
    printer.print(exprs)
    	=> "(* (- 123) (group 45.67))"

---
+++ rubolox/lib/rubolox/ast_printer.rb
@@ -0,0 +1,30 @@
+module Rubolox
+  class AstPrinter < Expr::Visitor
+    def print(expr)
+      expr.accept(self)
+    end
+
+    def visit_binary_expr(expr)
+      parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    end
+
+    def visit_grouping_expr(grouping)
+      parenthesize("group", grouping.expression)
+    end
+
+    def visit_literal_expr(literal)
+      return "nil" if literal.value.nil?
+      literal.value.to_s
+    end
+
+    def visit_unary_expr(unary)
+      parenthesize(unary.operator.lexeme, unary.right)
+    end
+
+    private
+
+    def parenthesize(name, *exprs)
+      "(#{name} #{exprs.map { |e| e.accept(self) }.join(" ")})"
+    end
+  end
+end
