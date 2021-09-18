2b2029d3f3530a25b74cce86e069c461863db0f6
Balazs Kutil <balazs@kutilovi.cz>
2021-09-18 21:10:49 +0200

    Chaputer 7.2.2, page ?? - evaluating parentheses

--- rubolox/lib/rubolox/interpreter.rb
+++ rubolox/lib/rubolox/interpreter.rb
@@ -3,5 +3,15 @@ module Rubolox
     def visit_literal_expr(literal)
       literal.value
     end
+
+    def visit_grouping_expr(grouping)
+      evaluate(grouping.expression)
+    end
+
+    private
+
+    def evaluate(expr)
+      expr.accept(self)
+    end
   end
 end
