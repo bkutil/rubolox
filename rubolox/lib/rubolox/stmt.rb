d40a95f6657e18a3c1dd77928d1992571e422a7b
Balazs Kutil <balazs@kutilovi.cz>
2021-09-27 22:03:30 +0200

    Chapter 8.1.1, page XX - statement syntax trees

---
+++ rubolox/lib/rubolox/stmt.rb
@@ -0,0 +1,42 @@
+module Rubolox
+  class Stmt
+    class Visitor
+      def visit_expression_stmt(stmt)
+        raise 'To be implemented'
+      end
+
+      def visit_print_stmt(stmt)
+        raise 'To be implemented'
+      end
+
+    end
+
+    class Expression < Stmt
+      attr_reader :expression
+
+      def initialize(expression)
+        @expression = expression
+      end
+
+      def accept(visitor)
+        visitor.visit_expression_stmt(self)
+      end
+    end
+
+    class Print < Stmt
+      attr_reader :expression
+
+      def initialize(expression)
+        @expression = expression
+      end
+
+      def accept(visitor)
+        visitor.visit_print_stmt(self)
+      end
+    end
+
+    def accept(visitor)
+      raise 'To be implemented'
+    end
+  end
+end
