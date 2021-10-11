c4dd2b76e70ff4ee23fb07b88c0c39ac2395ea1c
Balazs Kutil <balazs@kutilovi.cz>
2021-10-11 22:23:14 +0200

    Chapter 11.3.1, page 179 - resolving blocks

--- rubolox/lib/rubolox/resolver.rb
+++ rubolox/lib/rubolox/resolver.rb
@@ -3,10 +3,40 @@ module Rubolox
     include Expr::Visitor
     include Stmt::Visitor
 
-    attr_accessor :interpreter
-
     def initializer(interpreter)
       @interpreter = interpreter
+      @scopes = []
+    end
+
+    def resolve(statements)
+      statements.each do |statement|
+        resolve(statement)
+      end
+    end
+
+    def visit_block_stmt(stmt)
+      begin_scope
+      resolve(stmt.statements)
+      end_scope
+      nil
+    end
+
+    private
+
+    def begin_scope
+      scopes.push {}
+    end
+
+    def end_scope
+      scopes.pop
+    end
+
+    attr_accessor :interpreter, :scopes
+
+    # BK: without overloading there's one resolve method for both statements
+    # and expressions.
+    def resolve(stmt_or_expression)
+      stmt_or_expression.accept(self)
     end
   end
 end
